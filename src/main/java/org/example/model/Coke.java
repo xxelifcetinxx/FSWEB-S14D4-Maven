941bff46799b5dcbb135b7a58357406d97b8a1b7
Elif Cetin <xxelifcetinxx@gmail.com>
2025-05-15 13:44:12 +0300

    Coke ok

--- src/main/java/org/example/model/Coke.java
+++ src/main/java/org/example/model/Coke.java
@@ -1,4 +1,20 @@
 package org.example.model;
 
-public class Coke {
+public class Coke extends ProductForSale {
+    private boolean isDiet;
+
+    public Coke(String type, double price, String description, boolean isDiet) {
+        super(type, price, description);
+        this.isDiet = isDiet;
+    }
+
+    public boolean isDiet() {
+        return isDiet;
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println("[Coke] Type: " + getType() + ", Price: " + getPrice() +
+                ", Description: " + getDescription() + ", Diet: " + (isDiet ? "Yes" : "No"));
+    }
 }
