a35ac992d14b80f39b47dfb4d2d3e70b592e9ef6
Elif Cetin <xxelifcetinxx@gmail.com>
2025-05-15 13:46:32 +0300

    Bread ok

--- src/main/java/org/example/model/Bread.java
+++ src/main/java/org/example/model/Bread.java
@@ -1,4 +1,20 @@
 package org.example.model;
 
-public class Bread {
+public class Bread extends ProductForSale {
+    private String flourType;
+
+    public Bread(String type, double price, String description, String flourType) {
+        super(type, price, description);
+        this.flourType = flourType;
+    }
+
+    public String getFlourType() {
+        return flourType;
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println("[Bread] Type: " + getType() + ", Price: " + getPrice() +
+                ", Description: " + getDescription() + ", Flour Type: " + flourType);
+    }
 }
