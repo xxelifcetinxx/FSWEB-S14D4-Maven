26e8108bde543a9f9d2b51d013ef17aebf375bc2
Elif Cetin <xxelifcetinxx@gmail.com>
2025-05-15 13:43:44 +0300

    Chocolate ok

--- src/main/java/org/example/model/Chocolate.java
+++ src/main/java/org/example/model/Chocolate.java
@@ -1,4 +1,20 @@
 package org.example.model;
 
-public class Chocolate {
+public class Chocolate extends ProductForSale {
+    private int cocoaPercentage;
+
+    public Chocolate(String type, double price, String description, int cocoaPercentage) {
+        super(type, price, description);
+        this.cocoaPercentage = cocoaPercentage;
+    }
+
+    public int getCocoaPercentage() {
+        return cocoaPercentage;
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println("[Chocolate] Type: " + getType() + ", Price: " + getPrice() +
+                ", Description: " + getDescription() + ", Cocoa %: " + cocoaPercentage);
+    }
 }
