c4352491abd2e593779546a18578ed0d4d161b25
Elif Cetin <xxelifcetinxx@gmail.com>
2025-05-15 13:56:54 +0300

    Bread.java modified

--- src/main/java/org/example/model/Bread.java
+++ src/main/java/org/example/model/Bread.java
@@ -3,18 +3,21 @@ package org.example.model;
 public class Bread extends ProductForSale {
     private String flourType;
 
+    public Bread(String type, double price, String description) {
+        super(type, price, description);
+        this.flourType = "Standard Flour"; // varsayılan değer
+    }
+
     public Bread(String type, double price, String description, String flourType) {
         super(type, price, description);
         this.flourType = flourType;
     }
 
-    public String getFlourType() {
-        return flourType;
-    }
-
     @Override
     public void showDetails() {
-        System.out.println("[Bread] Type: " + getType() + ", Price: " + getPrice() +
-                ", Description: " + getDescription() + ", Flour Type: " + flourType);
+        System.out.println("Bread [type=" + getType() +
+                ", price=" + getPrice() +
+                ", description=" + getDescription() +
+                ", flourType=" + flourType + "]");
     }
 }
